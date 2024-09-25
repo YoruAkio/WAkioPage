00b1e7e62aa758c352203e5619ba94f119cb61c2
Akio <97880708+YoruAkio@users.noreply.github.com>
2024-09-25 15:09:07 +0000

    feat(page): Enhance SEO with unified metadata management

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,46 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>YoruAkio Portfolio</title>
+                <meta property="og:title" content="YoruAkio Portfolio" />
+                <meta
+                    property="og:description"
+                    content="Yoru Akio is a self-taught student developer from Indonesia. He is a fullstack developer who loves to build something cool about technology."
+                />
+                <meta
+                    property="og:image"
+                    content="https://yoruakio.tech/assets/kio.jpg"
+                />
+                <meta property="og:url" content="https://yoruakio.tech" />
+                <meta property="og:type" content="website" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="YoruAkio Portfolio" />
+                <meta
+                    name="twitter:description"
+                    content="Yoru Akio is a self-taught student developer from Indonesia. He is a fullstack developer who loves to build something cool about technology."
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://yoruakio.tech/assets/kio.jpg"
+                />
+                <meta name="twitter:site" content="@yoruakio" />
+
+                <meta
+                    name="description"
+                    content="Yoru Akio is a self-taught student developer from Indonesia. He is a fullstack developer who loves to build something cool about technology."
+                />
+                <meta
+                    name="keywords"
+                    content="next, nextjs, react, tailwind, daisyui, yoru akio, yoruakio, github, developer, growtopia, gtps, gtps-github, student developer, indonesia, indonesian, web developer, fullstack developer"
+                />
+                <meta name="author" content="Yoru Akio" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,44 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <meta
-                    property="og:title"
-                    content="Yoru Akio - Fullstack Developer"
-                />
-                <meta
-                    property="og:description"
-                    content="Yoru Akio is a self-taugh student developer from indonesia. He is a fullstack developer who loves to build something cool about technology."
-                />
-                <meta
-                    property="og:image"
-                    content="https://yourwebsite.com/path/to/image.jpg"
-                />
-                <meta property="og:url" content="https://yoruakio.tech" />
-                <meta property="og:type" content="website" />
-
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta
-                    name="twitter:title"
-                    content="Yoru Akio - Fullstack Developer"
-                />
-                <meta
-                    name="twitter:description"
-                    content="Yoru Akio is a self-taugh student developer from indonesia. He is a fullstack developer who loves to build something cool about technology."
-                />
-                <meta
-                    name="twitter:image"
-                    content="https://yourwebsite.com/path/to/image.jpg"
-                />
-                <meta name="twitter:site" content="@yoruakio" />
-
-                <meta
-                    name="description"
-                    content="Yoru Akio is a self-taugh student developer from indonesia. He is a fullstack developer who loves to build something cool about technology."
-                />
-                <meta name="keywords" content="next, nextjs, react, tailwind, daisyui, yoru akio, yoruakio, github, developer, growtopia, gtps, gtps-github, student developer, indonesia, indonesian, web developer, fullstack developer" />
-                <meta name="author" content="Yoru Akio" />
-            </Head>
+            <Head />
             <body className="antialiased">
                 <Main />
                 <NextScript />
